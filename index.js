0f27f7b7eeacb750b0c135b5f95804c75acaa4d3
likithreddy1 <likithreddy123@gmail.com>
2022-03-24 17:48:24 +0530

    updated

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ app.use('/messages',MessagesRouter)
 
 //operations
 app.get('/',(req,res)=>{
-    res.send("Hello there!!!")
+    res.send("Hello there!!! likith here")
 })
 app.listen(process.env.PORT || 3000, () => {
     console.log("port is running")
